c07e1ab6f8ca016011c49809bb392fc2b0b4e2ea
20tk11 <tomaskanapickass@gmail.com>
2022-09-26 11:16:05 +0300

    reads only txt files

--- Task.java
+++ Task.java
@@ -4,37 +4,38 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class Task{
+public class Task {
     public static void main(String[] args) throws IOException {
 
         File dir = new File("Files");
         File[] files = dir.listFiles();
         for (File file : files) {
-            if(file.isFile()) {
+            if (file.isFile() && file.getName().toLowerCase().endsWith(".txt")) {
                 BufferedReader inputStream = null;
                 String line;
-                try{
+                try {
                     inputStream = new BufferedReader(new FileReader(file));
-                    while((line = inputStream.readLine()) != null){
-                        
-                        String lineNumbers = line.replaceAll("[^0-9]+", " "); // replace letter sections with emptys spaces
-                        //System.out.println(Arrays.asList(lineNumbers.trim().split(" "))); // extract all numbers from array to an array
-                        for(String i: Arrays.asList(lineNumbers.trim().split(" "))){
-                            if(Integer.parseInt(i) >= 10){
+                    while ((line = inputStream.readLine()) != null) {
+
+                        String lineNumbers = line.replaceAll("[^0-9]+", " "); // replace letter sections with emptys
+                                                                              // spaces
+                        // System.out.println(Arrays.asList(lineNumbers.trim().split(" "))); // extract
+                        // all numbers from array to an array
+                        for (String i : Arrays.asList(lineNumbers.trim().split(" "))) {
+                            if (Integer.parseInt(i) >= 10) {
                                 System.out.println(line);
                             }
                         }
                     }
-                } catch (IOException e){
+                } catch (IOException e) {
                     System.out.println(e);
-                }
-                finally {
+                } finally {
                     if (inputStream != null) {
                         inputStream.close();
                     }
                 }
             }
         }
-        
+
     }
 }
