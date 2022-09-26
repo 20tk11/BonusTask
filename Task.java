99c6cb2d887f62c2afd9ba973d296f53dce9e1d6
20tk11 <tomaskanapickass@gmail.com>
2022-09-26 10:18:48 +0300

    can read multiple files from Files directory

--- Task.java
+++ Task.java
@@ -1,29 +1,40 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
 public class Task{
-    public static void main(String[] args) {
-        BufferedReader reader;
+    public static void main(String[] args) throws IOException {
 
-        try{
-            reader = new BufferedReader(new FileReader(
-                "input.txt"));
-            String line = reader.readLine();
-            while(line != null){
-                
-                String lineNumbers = line.replaceAll("[^0-9]+", " "); // replace letter sections with emptys spaces
-                //System.out.println(Arrays.asList(lineNumbers.trim().split(" "))); // extract all numbers from array to an array
-                for(String i: Arrays.asList(lineNumbers.trim().split(" "))){
-                    if(Integer.parseInt(i) >= 10){
-                        System.out.println(line);
+        File dir = new File("Files");
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            if(file.isFile()) {
+                BufferedReader inputStream = null;
+                String line;
+                try{
+                    inputStream = new BufferedReader(new FileReader(file));
+                    while((line = inputStream.readLine()) != null){
+                        
+                        String lineNumbers = line.replaceAll("[^0-9]+", " "); // replace letter sections with emptys spaces
+                        //System.out.println(Arrays.asList(lineNumbers.trim().split(" "))); // extract all numbers from array to an array
+                        for(String i: Arrays.asList(lineNumbers.trim().split(" "))){
+                            if(Integer.parseInt(i) >= 10){
+                                System.out.println(line);
+                            }
+                        }
+                    }
+                } catch (IOException e){
+                    System.out.println(e);
+                }
+                finally {
+                    if (inputStream != null) {
+                        inputStream.close();
                     }
                 }
-                line = reader.readLine();
             }
-        } catch (IOException e){
-            e.printStackTrace();
         }
+        
     }
 }
